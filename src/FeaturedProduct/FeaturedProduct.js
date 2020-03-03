a74ec734b40fb11fbdd0000f1e3162055becac55
Hunter Treadaway <51298555+huntertread@users.noreply.github.com>
2020-03-03 09:19:02 -0800

    added useState hook to featured product selector

--- src/FeaturedProduct/FeaturedProduct.js
+++ src/FeaturedProduct/FeaturedProduct.js
@@ -1,19 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import sampleData from '../sampledata.js'
 import './featuredproduct.css'
 
 const FeaturedProduct = (props) => {
+
+  const [featured, setFeatured] = useState(sampleData[1].age)
+
   return(
     <div className="featuredcontainer">
       <div className="mainproduct">
-        <h1>{sampleData[0].age}</h1>
+        <h1>{featured}</h1>
       </div>
       <div className="sideproducts">
         <ul>
-          <li>{sampleData[1].age}</li>
-          <li>{sampleData[2].age}</li>
-          <li>{sampleData[3].age}</li>
-          <li>{sampleData[4].age}</li>
+          <li onClick={() => setFeatured(sampleData[0].age)}>{sampleData[0].age}</li>
+          <li onClick={() => setFeatured(sampleData[1].age)}>{sampleData[1].age}</li>
+          <li onClick={() => setFeatured(sampleData[2].age)}>{sampleData[2].age}</li>
+          <li onClick={() => setFeatured(sampleData[3].age)}>{sampleData[3].age}</li>
+          <li onClick={() => setFeatured(sampleData[4].age)}>{sampleData[4].age}</li>
         </ul>
       </div>
     </div>
