f3770d6cd005616b411e9aeb65332de7f6a96e0c
Hunter Treadaway <51298555+huntertread@users.noreply.github.com>
2020-03-04 15:14:18 -0800

    stubbed out accordion conditional rendering with hooks

--- src/Accordion/AccordionWrapper.js
+++ src/Accordion/AccordionWrapper.js
@@ -1,13 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './accordionwrapper.css'
 
 const AccordionWrapper = (props) => {
-  return(
-    <div className="accordionwrapper">
-      <span>{props.containerName}</span>
-      <span>+</span>
-    </div>
-  )
+
+  const [selected, setSelect] = useState(false);
+
+  if (selected === false) {
+    return(
+      <div className="accordionwrapper">
+        <div className="accordionheader" onClick={() => setSelect(true)}>
+          <span>{props.containerName}</span>
+          <span>+</span>
+        </div>
+      </div>
+    )
+  }
+
+  if (selected === true) {
+    return(
+      <div className="accordionwrapper">
+        <div className="accordionheader" onClick={() => setSelect(false)}>
+          <span>{props.containerName}</span>
+          <span>-</span>
+        </div>
+        <div className="accordionbody">
+          <p>
+            ooooooooooooooooooooooooooooo
+            ooooooooooooooooooooooooooooo
+            ooooooooooooooooooooooooooooo
+            ooooooooooooooooooooooooooooo
+            ooooooooooooooooooooooooooooo
+          </p>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default AccordionWrapper
